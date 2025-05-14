37010819c3e2f079cebf7686e9ef6799aa271073
marilena <marilena_1@hotmail.com>
2025-05-14 18:07:21 +0200

    feat: add gitignore, readme and hello world class

---
+++ src/gr/aueb/cf/helloapp/HelloWorld.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.helloapp;
+
+/**
+ * This project is created to show how
+ * to make a git project, how to stage
+ * and commit changes in a local repo,
+ * ags well as how to push to GitHub.
+ */
+
+public class HelloWorld {
+
+}
